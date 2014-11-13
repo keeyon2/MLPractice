7f221ea6433f9a60984a4631bf423d54cb93950d
Keeyon Ebrahimi <keeyon2@gmail.com>
2014-11-13 17:55:08 -0500

    Added a new tree type that can have a leaf of anytime or nodes that can have as many children as they want, not just 2 like in a binary tree

--- MLFunctions.ml
+++ MLFunctions.ml
@@ -2,6 +2,9 @@ val TestListInt = [5, 1, 6, 13, 5, 6, 7, 76, 4]
 val TestListReal = [5.3, 2.6, 1.7, 3.2, 7.8, 10.2, 66.2] 
 val TestListListInt = [[1, 9, 3, 6], [1], [2, 4, 6], [5,5]]
 
+(* Given a partician int.  This returns a tuple with (ListLessThanPart,
+ * ListGreatern than Part*)
+
 fun ParticianInt x [] (ListLess : int list, ListMore : int list) = 
     (ListLess, ListMore)
  | ParticianInt x (origFirst::origRest) (ListLess : int list, ListMore : int list) = 
@@ -19,6 +22,7 @@ fun intPartitionSort (x::xs) =
      end
  | intPartitionSort _ = []
 
+(* Takes in operator to make the partician work will all types *)
 fun ParticianPoly (op <) x [] (ListLess, ListMore) = 
     (ListLess, ListMore)
  | ParticianPoly (op <) x (origFirst::origRest) (ListLess, ListMore) = 
@@ -36,3 +40,10 @@ fun partitionSort (op <) (x::xs) =
      end
  | partitionSort (op <) _ = []
 
+ datatype 'a tree = leaf of 'a | node of 'a tree list
+
+val myTree = node [node [node [leaf [4,2,14],leaf [9,83,32],leaf [96,123,4]],
+                              node [leaf [47,71,82]],node [leaf [19,27,10],
+                                                           leaf [111,77,22,66]],
+                              leaf [120,42,16]],
+                        leaf [83,13]]
